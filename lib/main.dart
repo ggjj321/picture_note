801a502e01e6f8938b8f16a6e2744aaf27f300aa
ggjj321 <10370324lsjh@gmail.com>
2021-11-17 23:04:50 +0800

    clean11/17

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,18 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:picture_note/page/camera.dart';
 import 'package:picture_note/page/home.dart';
-import 'package:picture_note/page/takepicture.dart';
+
 
 Future<void> main() async {
   // Ensure that plugin services are initialized so that `availableCameras()`
   // can be called before `runApp()`
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
-
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-
-  runApp(MyApp(camera: firstCamera));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final CameraDescription camera;
-
   const MyApp({
     Key? key,
-    required this.camera,
   }) : super(key: key);
 
   @override
@@ -32,12 +21,12 @@ class MyApp extends StatelessWidget {
       initialRoute: home.routeName,
       routes: {
         home.routeName: (context) => const home(),
-        TakePictureScreen.routeName: (context) => TakePictureScreen(
-          camera: camera,
-        ),
-        CameraApp.routeName:(context) => CameraApp(
-          camera: camera,
-        ),
+        // TakePictureScreen.routeName: (context) => TakePictureScreen(
+        //   camera: camera,
+        // ),
+        // CameraApp.routeName:(context) => CameraApp(
+        //   camera: camera,
+        // ),
       },
     );
   }
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -2,9 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:picture_note/page/takepicture.dart';
-import 'package:picture_note/page/camera.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -53,34 +50,5 @@ class _homeState extends State<home> {
   }
 }
 
-// class home extends StatelessWidget {
-//   static const routeName = '/home';
-//   static const List<String> entries = <String>['A', 'B', 'C'];
-//   static const List<int> colorCodes = <int>[600, 500, 100];
-//
-//   const home({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: null,
-//       body:ListView.separated(
-//         padding: const EdgeInsets.all(8),
-//         itemCount: entries.length,
-//         itemBuilder: (BuildContext context, index) {
-//           return ElevatedButton(
-//             onPressed: () async{
-//               final ImagePicker _picker = ImagePicker();
-//               var image = await _picker.pickImage(source: ImageSource.camera);
-//               setState(() {});
-//             },
-//             child: const Text('Enabled'),
-//           );
-//         },
-//         separatorBuilder: (BuildContext context, int index) => const Divider(),
-//       ),
-//     );
-//   }
-// }
 
 
